f331d280283970ebdc0966c989a6c15fc3e7d803
Grace Smyth <gracemurphy45@gmail.com>
2024-03-05 19:06:44 +0000

    week 2

---
+++ topic 2- statements/lab 2.2 First programs.py
@@ -0,0 +1,29 @@
+# helloworld.py
+# this programme just prints out Hello world
+# Author: Grace Mary Smyth
+
+'''
+print ("Hello World")
+'''
+'''
+print("Hello")
+'''
+'''
+print("Hello Grace")
+'''
+'''
+# multiply.py
+answer = 111 * 555
+print(111*555)
+print(answer)
+'''
+'''
+# Hello2.py
+name = input("Enter your name:")
+print("Hello " + name + 
+      ", Nice to meet you!")
+      '''
+# addOne.py
+number = int(input("Please enter a number:"))
+newNumber = number + 1
+print (f'{number} plus one is {newNumber}')
