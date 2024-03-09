06bb9f15166b70c6bd056a2051752782d3e8eaaf
Grace Smyth <gracemurphy45@gmail.com>
2024-03-09 19:21:47 +0000

    subtracting two numbers

---
+++ Topic 3-Variables/lab 3.1 Variables and state/sub.py
@@ -0,0 +1,9 @@
+# Programme to sutract one number from another
+
+# input reads in a string so we need to convert it into an integer so we can perform mathematical operations
+# Author: Grace Mary Smyth
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+answer= x-y
+print("{} minus {} is {} ".format(x, y, answer))
