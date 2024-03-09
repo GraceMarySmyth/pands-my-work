34c526a5eee6c99b5828381f8ba2a6656f55407f
Grace Smyth <gracemurphy45@gmail.com>
2024-03-09 19:13:29 +0000

    First part of lab3.1

--- Topic 3-Variables/messingwithtypes.py
+++ Topic 3-Variables/messingwithtypes.py
@@ -1,5 +1,9 @@
 # Messing with variable types
 # Author: Grace Mary Smyth
 
+#answer = asdf
 answer = 12
+
 print (f"answer is {answer}")
+print (answer is "+ str(answer)")
+print (f"type of answer is {type(answer)}")
