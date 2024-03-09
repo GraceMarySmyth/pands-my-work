1629047158cf6ab722b069d4cae89b234aa8b780
Grace Smyth <gracemurphy45@gmail.com>
2024-03-09 19:51:27 +0000

    random generating numbers

---
+++ Topic 3-Variables/lab 3.1 Variables and state/randomGenerator.py
@@ -0,0 +1,7 @@
+# programme that prints out a random number between 1 and 10
+# Author: Grace Mary Smyth
+
+import random
+
+number = random.randint(1,10)
+print("Here is a random number {}".format(number))
