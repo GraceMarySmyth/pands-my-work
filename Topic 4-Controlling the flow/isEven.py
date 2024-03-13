12c60f86da98a329630dd79088a00dbcd6862816
Grace Smyth <gracemurphy45@gmail.com>
2024-03-13 09:40:53 +0000

    Lab4.1

---
+++ Topic 4-Controlling the flow/isEven.py
@@ -0,0 +1,11 @@
+# A programme that asks user to enter in anumber 
+# Programme will tell if number is even or odd
+# Author: Grace Mary Smyth
+
+number = int(input("Enter an Integer: ")) 
+
+if (number % 2) == 0:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
+
