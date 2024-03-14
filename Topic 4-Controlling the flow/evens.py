2c8a9210b3b44fef44a3dd4e67dbfe0d5274ebac
Grace Smyth <gracemurphy45@gmail.com>
2024-03-14 19:17:14 +0000

    while loop lab 4.2.1

---
+++ Topic 4-Controlling the flow/evens.py
@@ -0,0 +1,10 @@
+# prints out all the even numbers from 2 to 100
+# Author: Grace Mary Smyth
+
+numberto = 100
+evennum = 2
+
+while evennum <= numberto:
+    print (evennum)
+    evennum += 2
+    
