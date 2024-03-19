fc94abc921c8bcd19e94c592dba0d4b5d76a9707
Grace Smyth <gracemurphy45@gmail.com>
2024-03-19 12:59:30 +0000

    lab 5.2 tuples

---
+++ Topic 5- datastructures/tuplesummermts.py
@@ -0,0 +1,28 @@
+# Create a tuple that stores months of the year
+# create a second tuple with just the summer months
+# print the summer months one at a time
+
+# Grace Mary Smyth
+
+months = ("January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "Septhember",
+          "October",
+          "November",
+          "December"
+) 
+'''
+summer = months[4:7]
+for month in summer:
+    print(month)
+    '''
+
+autumn = months[7:10] 
+for month in autumn:
+    print(month)   
