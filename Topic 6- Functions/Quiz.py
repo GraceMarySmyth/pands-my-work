425f631fdca799cd88ca442b1c4743ac9a65243c
Grace Smyth <gracemurphy45@gmail.com>
2024-03-27 13:42:15 +0000

    lab 6

---
+++ Topic 6- Functions/Quiz.py
@@ -0,0 +1,10 @@
+# Quiz
+# Author: Grace Mary Smyth
+
+# function yo takes one parameter
+def yo(a):
+    return a * 2
+# Here we are calling the function yo
+# and are passing in an argument, 3
+yo(3)
+print(yo(3))
---
+++ Topic 6- Functions/studentmgementprog.py
@@ -0,0 +1,61 @@
+# Create a programme to allow user to create new students and to view students
+# Big programme!
+# Author: Grace Mary Smyth
+
+def displaymenu():
+    print("What would you like to do?")
+    print("\t(a) Add new student")
+    print("\t(v) View student")
+    print("\t(q) Quit")
+    choice = input("Type one letter (a/v/q): ").strip()
+    return choice 
+# test the function
+'''
+choice = displaymenu()
+print(f"you chose: {choice}")
+'''
+def doadd(students):
+    currentstudent = {}
+    currentstudent["name"] = input("Enter name: ")
+    currentstudent["modules"] = readmodules()
+    students.append(currentstudent)
+
+def readmodules():
+    modules = []
+    modulename = input("\tEnter the first module name (blank to quit) :").strip()
+
+    while modulename != "":
+        module = {}
+        module["name"] = modulename
+        # error handling
+        module["grade"]=int(input("\t\tEnter grade: "))
+        modules.append(module)
+        # now read the next module name
+        modulename = input("\tEnter the next module name (blank to quit) :").strip()
+    return modules
+
+def doview(students):
+    # Fill in later
+    print(students)
+
+# Main programme
+students = []
+choice = displaymenu()
+while(choice != 'q'):
+    if choice == 'a':
+        doadd(students)
+    elif choice == 'v':
+        doview(students)
+    elif choice != 'q':
+        print("\n\nplease select either a, v or q")
+    choice = displaymenu()
+'''
+# Test
+doadd(students) 
+doadd(students)
+print(students)
+'''
+
+students = []
+def readmodules():
+    return []
