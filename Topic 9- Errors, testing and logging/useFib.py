7132452b128630d4149c06b038ff5409b8a5dbc1
Grace Smyth <gracemurphy45@gmail.com>
2024-04-18 20:08:35 +0100

    save actual code

---
+++ Topic 9- Errors, testing and logging/useFib.py
@@ -0,0 +1,8 @@
+# This program prompts the user for a number and
+# Prints out the fibonacci sequence of that many numbers
+# Author: Grace Mary Smyth
+
+import myFunctions
+
+nTimes = int(input("How many:"))
+print(myFunctions.fibonacci(nTimes))
